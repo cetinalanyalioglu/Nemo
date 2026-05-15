dbe822f364da453ae41765edc39339072ea9c92b
Cetin <cetinalanyalioglu@gmail.com>
2026-05-15 23:13:19 +0200

    Move save/load actions into the document pane File section.

--- src/components/NavigationControls.tsx
+++ src/components/NavigationControls.tsx
@@ -1,27 +1,13 @@
-import React, { useRef } from 'react';
-import { useNodeContext } from '../context/NodeContext';
+import React from 'react';
 import { useAppState } from '../context/AppStateContext';
-import { FaSave, FaFolderOpen } from 'react-icons/fa';
 import { IoDocumentTextOutline, IoLibrary } from 'react-icons/io5';
 import '../styles/navigation-controls.css';
 
 const NavigationControls = React.memo(() => {
-  const { saveToFile, loadFromFile } = useNodeContext();
-
   const {
     sidebar: { isOpen, activePane },
     actions,
   } = useAppState();
-
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      loadFromFile(file);
-      event.target.value = '';
-    }
-  };
 
   return (
     <div className="navigation-controls">
@@ -41,27 +27,6 @@ const NavigationControls = React.memo(() => {
         title="Node library"
       >
         <IoLibrary />
-      </button>
-
-      <button type="button" onClick={saveToFile} className="nav-button" title="Save canvas">
-        <FaSave />
-      </button>
-
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleFileSelect}
-        accept=".json"
-        style={{ display: 'none' }}
-      />
-
-      <button
-        type="button"
-        onClick={() => fileInputRef.current?.click()}
-        className="nav-button"
-        title="Load canvas"
-      >
-        <FaFolderOpen />
       </button>
     </div>
   );
